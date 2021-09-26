e84e1af2ca2a4ea614ec9b268fa3640411df490b
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-09-26 10:11:36 +0530

    jwt token with login api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,70 @@
+import react, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import jwt from "jsonwebtoken";
 
 export default function Home() {
+  const [username, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [message, setMessage] = useState<string>("You are not logged in");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!username || !password) {
+      alert("sexy");
+    } else {
+      const data = await fetch("/api/login", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: {
+          'Content-Type':'application/json'
+        }
+      }).then((res) => res.json());
+
+      let res = data.token;
+
+      if (res) {
+        const json = jwt.decode(res) as { [key: string]: string };
+        console.log(json);
+        setMessage(
+          `Welcome ${json.username} and you are ${
+            json.admin ? "an admin" : "not admin"
+          }`
+        );
+      } else {
+        setMessage("Something went wrong");
+      }
+    }
+  };
+
   return (
-    <div>Hi This my first project</div>
-  )
+    <div className={styles.container}>
+      <h1>{message}</h1>
+      <div className={`${styles.card} ${styles.grid}`}>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="enter username"
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 }
